a749eaa6cccbd7a52cc5178e22d57746f16213e3
mac stephens <macstephens@gmail.com>
2023-05-24 18:44:01 -0400

    beginning stretch goals

--- ghi/js/new-conference.js
+++ ghi/js/new-conference.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    const url = 'http://localhost:8000/api/states/';
+    const url = 'http://localhost:8000/api/locations/';
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -7,22 +7,22 @@ window.addEventListener('DOMContentLoaded', async () => {
         } else {
             const data = await response.json();
 
-            const selectTag = document.getElementById('state');
+            const selectTag = document.getElementById('location');
 
-            for (let state of data.states) {
+            for (let location of data.locations) {
                 const option = document.createElement('option');
-                option.value = state.abbreviation;
-                option.innerHTML = state.name;
+                option.value = location.id;
+                option.innerHTML = location.name;
                 selectTag.appendChild(option);
             }
 
-            const formTag = document.getElementById('create-location-form');
+            const formTag = document.getElementById('create-conference-form');
             formTag.addEventListener('submit', async event => {
                 event.preventDefault();
                 const formData = new FormData(formTag);
                 const json = JSON.stringify(Object.fromEntries(formData));
 
-                const locationUrl = 'http://localhost:8000/api/locations/';
+                const conferenceUrl = 'http://localhost:8000/api/conferences/';
                 const fetchConfig = {
                     method: "post",
                     body: json,
@@ -30,10 +30,10 @@ window.addEventListener('DOMContentLoaded', async () => {
                         'Content-Type': 'application/json',
                     },
                 };
-                const response = await fetch(locationUrl, fetchConfig);
+                const response = await fetch(conferenceUrl, fetchConfig);
                 if (response.ok) {
                     formTag.reset();
-                    const newLocation = await response.json();
+                    const newConference = await response.json();
                 }
             });
 
